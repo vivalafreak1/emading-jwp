3ab96149c94a13ee338005d9541675a1f0dea8f6
vivalafreak1 <arieftaufikrahman123@gmail.com>
2023-12-29 07:14:38 +0700

    minor fix

--- admin/template/header.php
+++ admin/template/header.php
@@ -116,7 +116,7 @@
                   </g>
                 </svg>
               </span>
-              <span class="app-brand-text demo menu-text fw-bolder ms-2">Sneat</span>
+              <span class="app-brand-text demo menu-text fw-bolder ms-2">JeWePe</span>
             </a>
 
             <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
