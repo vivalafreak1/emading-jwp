ee59bb74f06c2eb7c2ce870c2ca26d09e505eb33
vivalafreak1 <arieftaufikrahman123@gmail.com>
2024-01-02 16:21:48 +0700

    Show Article data on Admin

--- admin/config_query.php
+++ admin/config_query.php
@@ -1,7 +1,8 @@
 <?php
 // creating class with database name
 
-class database{
+class database
+{
     var $host = "localhost";
     var $username = "root";
     var $password = "";
@@ -16,17 +17,40 @@ class database{
             $this->password,
             $this->database
         );
-        if(mysqli_connect_errno()){
-            echo "Koneksi database gagal : ". mysqli_connect_error();
+        if (mysqli_connect_errno()) {
+            echo "Koneksi database gagal : " . mysqli_connect_error();
         }
     }
 
     //Get data tb_admin
-    public function get_data_admin($username) {
+    public function get_data_admin($username)
+    {
         $data = mysqli_query(
-            $this->connection,"SELECT * FROM tb_admin WHERE username='$username'" 
+            $this->connection,
+            "SELECT * FROM tb_admin WHERE username='$username'"
         );
         return $data;
+    }
+
+    //Get data tb_article
+    public function show_data()
+    {
+        $data = mysqli_query(
+            $this->connection,
+            "SELECT id_article, imageurl, title, content, ispublished, tba.created_at, tba.updated_at, name, tba.id_admin FROM tb_article tba JOIN tb_admin tbu on tba.id_admin = tbu.id_admin"
+        );
+
+        if ($data) {
+            if (mysqli_num_rows($data) > 0) {
+                while ($row = mysqli_fetch_array($data)) {
+                    $result[] = $row;
+                }
+            } else {
+                $result = '0';
+            }
+        }
+
+        return $result;
     }
 }
 ?>
--- admin/index.php
+++ admin/index.php
@@ -1,74 +1,86 @@
 <?php
 include("template/header.php");
+include("config_query.php");
+$db = new database();
+$data_article = $db->show_data();
 ?>
-            <!-- Content -->
+<!-- Content -->
 
-            <div class="container-xxl flex-grow-1 container-p-y">
-              <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> Basic Tables</h4>
+<div class="container-xxl flex-grow-1 container-p-y">
+  <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Manajemen /</span> Artikel</h4>
 
-              <!-- Responsive Table -->
-              <div class="card">
-                <h5 class="card-header">Responsive Table</h5>
-                <div class="table-responsive text-nowrap">
-                  <table class="table">
-                    <thead>
-                      <tr class="text-nowrap">
-                        <th>#</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                        <th>Table heading</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row">1</th>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">2</th>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">3</th>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                        <td>Table cell</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-              <!--/ Responsive Table -->
+  <!-- Responsive Table -->
+  <div class="card">
+    <div class="card-header">
+      <div class="card-title">
+        <div class="row">
+          <div class="col-lg-6">
+            <h5>Daftar Artikel</h5>
+          </div>
+          <div class="col-lg-6">
+            <div class="float-end">
+              <a href="tambah_data.php" class="btn btn-primary">
+                <i class="bx bx-plus"></i>
+                Tambah Data
+              </a>
             </div>
-            <!-- / Content -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive text-nowrap">
+        <table class="table">
+          <thead>
+            <tr class="text-nowrap">
+              <th class="text-center bg-primary text-white">No</th>
+              <th class="text-center bg-primary text-white">Gambar Header</th>
+              <th class="text-center bg-primary text-white">Judul Artikel</th>
+              <th class="text-center bg-primary text-white">Status Terbit</th>
+              <th class="text-center bg-primary text-white">Tanggal Update</th>
+              <th class="text-center bg-primary text-white">Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            if ($data_article == '0') {
+              echo '<tr><td>Data Tidak Tersedia!</td></tr>';
+            } else {
+              $number = 1;
+              foreach ($data_article as $row) {
+                ?>
+                <tr>
+                  <th>
+                    <?= $number++; ?>
+                  </th>
+                  <td>
+                    <?= $row['imageurl']; ?>
+                  </td>
+                  <td>
+                    <?= $row['title']; ?>
+                  </td>
+                  <td>
+                    <?= $row['ispublished']; ?>
+                  </td>
+                  <td>
+                    <?= $row['updated_at']; ?>
+                  </td>
+                  <td>
+                    <a href="edit.php" class="btn btn-sm btn-warning">Ubah</a>
+                    <a href="delete.php" class="btn btn-sm btn-danger">Hapus</a>
+                  </td>
+                </tr>
+              <?php
+              }
+            } ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+  <!--/ Responsive Table -->
+</div>
+<!-- / Content -->
 <?php
 include("template/footer.php");
 ?>
